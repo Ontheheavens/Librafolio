f045a583d1ba9d48942c24262220f21f5c95559a
Ontheheavens <l2van876b@gmail.com>
2024-05-08 22:13:01 +0400

    User class and table tweak.

--- src/main/java/oth/librafolio/model/User.java
+++ src/main/java/oth/librafolio/model/User.java
@@ -1,6 +1,12 @@
 package oth.librafolio.model;
 
 import jakarta.persistence.*;
+import org.hibernate.annotations.CreationTimestamp;
+import org.hibernate.annotations.UpdateTimestamp;
+
+import java.sql.Timestamp;
+import java.time.LocalDateTime;
+import java.util.Date;
 
 /**
  * @author Ontheheavens
@@ -16,8 +22,23 @@ public class User {
     private String username;
     private String password;
 
+    private String email;
+
+    @Column(columnDefinition = "boolean default true", nullable = false)
+    private boolean enabled;
+    @Column(columnDefinition = "boolean default false", nullable = false)
+    private boolean tokenExpired;
+
     @Enumerated(EnumType.STRING)
     private Role role;
+
+    @CreationTimestamp
+    @Column(name = "created_at", nullable = false, updatable = false)
+    private Timestamp createdAt;
+
+    @UpdateTimestamp
+    @Column(name = "updated_at", nullable = false)
+    private Timestamp updatedAt;
 
     public User() {
 
@@ -52,6 +73,30 @@ public class User {
 
     public void setRole(Role inputRole) {
         this.role = inputRole;
+    }
+
+    public String getEmail() {
+        return email;
+    }
+
+    public void setEmail(String email) {
+        this.email = email;
+    }
+
+    public boolean isEnabled() {
+        return enabled;
+    }
+
+    public void setEnabled(boolean enabled) {
+        this.enabled = enabled;
+    }
+
+    public boolean isTokenExpired() {
+        return tokenExpired;
+    }
+
+    public void setTokenExpired(boolean tokenExpired) {
+        this.tokenExpired = tokenExpired;
     }
 
 }
